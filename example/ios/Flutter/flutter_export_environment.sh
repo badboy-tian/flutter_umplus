cafa406f8d8e6084bf7ba51b54eb8753f4d8b48e
qiujietian <tqj.zyy@gmail.com>
2020-08-22 21:25:05 +0800

    修改依赖

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tian/Documents/work/sdks/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tian/Documents/work/android/flutter_umplus/example"
+export "FLUTTER_ROOT=/Users/qiujietian/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/qiujietian/Documents/work/Android/flutter_umplus/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/tian/Documents/work/sdks/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/qiujietian/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
